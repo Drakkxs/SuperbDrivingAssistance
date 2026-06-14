9146ef773cfc447a9d3c3d2930a600bf41d83892
Drakkxs <70417194+Drakkxs@users.noreply.github.com>
2026-06-14 11:15:16 -0400

    Minor misc update.

--- server_scripts/SuperbDrivingAssistance.js
+++ server_scripts/SuperbDrivingAssistance.js
@@ -1,10 +1,12 @@
 // Visit the wiki for more info - https://kubejs.com/
 // requires: superbwarfare
 
+const { $Quaternion } = require("@package/gg/essential/model/util");
+
 (() => {
 
     const { $Double } = require("@package/java/lang");
-    const { $Vector3d } = require("@package/org/joml");
+    const { $Vector3d, $Vector2d } = require("@package/org/joml");
     const { $OBB, $TestTool } = require("@package/com/atsuishio/superbwarfare/tools");
     const { $ClipContext } = require("@package/net/minecraft/world/level");
     const { $UUID } = require("@package/java/util");
@@ -45,6 +47,7 @@
     })
 
 
+
     NativeEvents.onEvent($EntityTickEvent$Pre, (event) => {
 
         let driver = event.getEntity();
@@ -81,90 +84,60 @@
      */
     function tickVehicle(vehicle, driver) {
 
-        // let OBBs = vehicle.getOBBs();
-        // let obbBOX = {
-        //     center: OBBs.getFirst().center,
-        //     min: $Vector3d($Double.MAX_VALUE, $Double.MAX_VALUE, $Double.MAX_VALUE),
-        //     max: $Vector3d(-$Double.MAX_VALUE, -$Double.MAX_VALUE, -$Double.MAX_VALUE)
-        // }
-
-        // OBBs.forEach((OBB) => {
-        //     var pos = OBB.center
-        //     obbBOX.max = obbBOX.max.max(pos)
-        //     obbBOX.min = obbBOX.min.min(pos)
-
-        //     obbBOX.center.half(obbBOX.max, obbBOX.min)
-        // })
-
         if (vehicle.enableAABB()) return;
 
         let obbList = vehicle.getOBBs()
 
-        // let min = new $Vector3d($Double.MAX_VALUE, $Double.MAX_VALUE, $Double.MAX_VALUE)
-        // let max = new $Vector3d(-$Double.MAX_VALUE, -$Double.MAX_VALUE, -$Double.MAX_VALUE)
 
         let min = new $Vector3d($Double.MAX_VALUE, $Double.MAX_VALUE, $Double.MAX_VALUE)
         let max = new $Vector3d(-$Double.MAX_VALUE, -$Double.MAX_VALUE, -$Double.MAX_VALUE)
+        let vehicleRotation = vehicle.getRotationVector();
 
-
-        // if (vehicle.tickCount % 10 !== 0) vehicle.level.runCommandSilent(`execute at ${vehicle.getStringUuid()} run teleport ${vehicle.getStringUuid()} ~ ~ ~ ${vehicle.tickCount} 0`)
-
-
+        // Incorperate vehicle bounding box
+        min = vehicle.getBoundingBox().getMinPosition();
+        max = vehicle.getBoundingBox().getMaxPosition();
 
         /** @param {$OBB} obb */
         function obbThing(obb) {
-            let center = obb.center
-            let vertices = obb.getVertices()
-            let axes = obb.getAxes()
-            let rotation = obb.rotation()
+            let center = obb.center;
+            let vertices = obb.getVertices();
+            let axes = obb.getAxes();
+            let rotation = obb.rotation();
 
-            // drawParticle("minecraft:glow", vehicle, center.x(), center.y(), center.z())
+
             vertices.forEach((vertex) => {
-                // let minx = Math.min(min.x(), Math.sin(rotation.x()) + vertex.x())
-                // let miny = Math.min(min.y(), Math.cos(rotation.y()) + vertex.y())
-                // let minz = Math.min(min.z(), Math.sin(rotation.z()) + vertex.z())
 
-                // let maxx = Math.max(max.x(), Math.sin(rotation.x()) + vertex.x())
-                // let maxy = Math.max(max.y(), Math.cos(rotation.y()) + vertex.y())
-                // let maxz = Math.max(max.z(), Math.sin(rotation.z()) + vertex.z())
+                let minx = Math.min(min.x(), Math.sin(rotation.x()) + vertex.x());
+                let miny = Math.min(min.y(), Math.cos(rotation.y()) + vertex.y());
+                let minz = Math.min(min.z(), Math.sin(rotation.z()) + vertex.z());
 
+                let maxx = Math.max(max.x(), Math.sin(rotation.x()) + vertex.x());
+                let maxy = Math.max(max.y(), Math.cos(rotation.y()) + vertex.y());
+                let maxz = Math.max(max.z(), Math.sin(rotation.z()) + vertex.z());
 
-                let minx = Math.min(min.x(), Math.sin(rotation.x()) + vertex.x())
-                let miny = Math.min(min.y(), Math.cos(rotation.y()) + vertex.y())
-                let minz = Math.min(min.z(), Math.sin(rotation.z()) + vertex.z())
-
-                let maxx = Math.max(max.x(), Math.sin(rotation.x()) + vertex.x())
-                let maxy = Math.max(max.y(), Math.cos(rotation.y()) + vertex.y())
-                let maxz = Math.max(max.z(), Math.sin(rotation.z()) + vertex.z())
-                // // drawParticle("minecraft:end_rod", vehicle, vertex.x(), vertex.y(), vertex.z())
-                // min.x = Math.min(min.x, (vertex.x() + rotation.x()))
-                // min.y = Math.min(min.y, (vertex.y() + rotation.y()))
-                // min.z = Math.min(min.z, (vertex.z() + rotation.z()))
-
-                // max.x = Math.max(max.x, (vertex.x() + rotation.x()))
-                // max.y = Math.max(max.y, (vertex.y() + rotation.y()))
-                // max.z = Math.max(max.z, (vertex.z() + rotation.z()))
-
-                min = new $Vector3d(minx, miny, minz)
-                max = new $Vector3d(maxx, maxy, maxz)
+                min = new $Vector3d(minx, miny, minz);
+                max = new $Vector3d(maxx, maxy, maxz);
             });
         }
 
-        // obbThing(obbList.getFirst())
         obbList.forEach((obb) => {
             obbThing(obb)
         });
 
+        let width = (max.x() - min.x()) + Math.sin(vehicleRotation.x);
+        let height = (max.y() - min.y()) + Math.cos(vehicleRotation.y);
+        let depth = (max.z() - min.z()) + Math.sin(vehicleRotation.x);
+
+
         let postion = vehicle.position()
         let aabb = new $AABB($OBB.vector3dToVec3(min), $OBB.vector3dToVec3(max))
-        let spot = aabb.getCenter().add(vehicle.getMotionX(), vehicle.getMotionY(), vehicle.getMotionZ())
-        let deltaMovement = vehicle.getDeltaMovement()
-        // let postionCombined = vehicle.position().add(spot.x(), spot.y(), spot.z());
-        // let aabb = $VehicleMotionUtils.INSTANCE.calculateCombinedAABBOptimized(vehicle).inflate(0.5);
-        // let aabb = AABB.of(obbBOX.min.x(), obbBOX.min.y(), obbBOX.min.z(), obbBOX.max.x(), obbBOX.max.y(), obbBOX.max.z()).move(obbBOX.center.x(), obbBOX.center.y(), obbBOX.center.z())
-        // drawParticle("minecraft:end_rod", vehicle, postionCombined.x(), postionCombined.y(), postionCombined.z());
+
+        let deltaMovement = vehicle.getDeltaMovement().scale(1)
+        let spot = aabb.getCenter().add(deltaMovement.x(), deltaMovement.y(), deltaMovement.z())
+
+
+        debugDriving(vehicle, `Vehicle AABB Size ${aabb.getSize()} Rotation: ${vehicleRotation.length()} Width: ${width} Height: ${height} Depth: ${depth}`)
         drawParticle("minecraft:end_rod", vehicle, aabb.getCenter().x(), aabb.getCenter().y(), aabb.getCenter().z())
-        drawAABB("minecraft:flame", vehicle, aabb)
         if (vehicle.tickCount % 60 !== 0) $TestTool.renderAABBEdgesWithParticles(vehicle.level, aabb, "minecraft:flame", 1, false)
 
     }
@@ -208,10 +181,6 @@
         vehicle.getLevel().runCommandSilent(
             `particle ${particle} ${x} ${y} ${z} 0 0 0 0 1 force`
         );
-
-        debugDriving(vehicle,
-            `x=${x} y=${y} z=${z}`
-        )
     }
 
 })();
